5249923d84c8335bd9846b8c54ff60ded08eb61e
putri jane <79836112+putrijane@users.noreply.github.com>
2021-03-14 14:16:12 +0700

    Add files via upload

---
+++ kalkulator.java
@@ -0,0 +1,17 @@
+public class kalkulator {
+	public static void main (String args []){
+		int a = 10, b=3;
+		
+		int jumlah = a+b;
+		int kurang = a-b;
+		int kali = a*b;
+		int bagi =a/b;
+		int modulus = a%b;
+		System.out.println("hasil dari penjulahan :" +jumlah);
+		System.out.println("hasil dari pengurangan :"+kurang);
+		System.out.println("hasil dari perkalian "+kali);
+		System.out.println("hasil dari pembagian :"+bagi);
+		System.out.println("hasil dari modulus : "+modulus);
+	}
+
+}
